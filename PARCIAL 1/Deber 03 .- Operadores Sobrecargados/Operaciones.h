4d52d20550ade567614814c3ec87da915019ab39
Josué Merino <yoshichicharra11@gmail.com>
2021-11-05 01:49:55 -0500

    Operadores Sobrecargados

---
+++ PARCIAL 1/Deber 03 .- Operadores Sobrecargados/Operaciones.h
@@ -0,0 +1,24 @@
+/* Universidad de las Fuerzas Armadas - ESPE
+   Software
+   Sobrecarga de Operadores
+   Autor:
+   -Jhoel David Chicaiza Collantes
+   -Ednan Josue Merino Calderon
+   -Daniel Alberto Vizcarra Gaibor
+   -Pablo David Yánez Alvear
+
+   Fecha de creación: 04/11/2021
+   Fecha de modificación: 05/11/2021
+*/
+
+#pragma once
+
+class Operaciones {
+public:
+	Operaciones();
+	~Operaciones();
+	int operator++(int);
+	int operator--(int);
+	int operator>>(int);
+	double operator>>(double);
+};
