3fa4badc5688f50335c4ba8a66909d04b6e333f8
Jhoel-Chicaiza7 <jhoel.david852456@gmail.com>
2021-11-05 12:56:58 -0500

    Unit Test Complejos v2

---
+++ PARCIAL 1/Trabajo en Clase 02 - Complejos/Complejo/Complejo.h
@@ -0,0 +1,35 @@
+/* Universidad de las Fuerzas Armadas - ESPE
+   Software
+   Complejos
+   Autor:
+   -Jhoel David Chicaiza Collantes
+   -Ednan Josue Merino Calderon
+   -Daniel Alberto Vizcarra Gaibor
+   -Pablo David Yánez Alvear
+
+   Fecha de creación: 05/11/2021
+   Fecha de modificación: 05/11/2021
+*/
+
+#pragma once
+#include <iostream>
+using namespace std;
+template <class T>
+class Complejo;
+template <typename T>
+ostream& operator <<(ostream& o, const Complejo<T>& A);
+
+template <class T>
+class Complejo {
+public:
+    Complejo(T real = 0, T img = 0);
+    void setReal(T real);
+    void setImg(T img);
+    T getReal();
+    T getImg();
+    Complejo<T>& operator +(const Complejo<T>& A);
+    Complejo<T>& operator -(const Complejo<T>& A);
+private:
+    T real;
+    T img;
+};
